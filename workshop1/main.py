d0c73e14db1a02e249fbe839ae2664f3afc5489d
Luka Tavkhelidze <tavkhelidzeluka@gmail.com>
2022-05-30 19:16:50 +0400

    Add workshop 1

---
+++ workshop1/main.py
@@ -0,0 +1,26 @@
+import decimal
+import math
+# print("I'm Luka")
+
+# print("21 Years old")
+
+# print("მე ვარ ლუკა")
+# print("ვისწავლოთ პითონი")
+
+print(5 + 1)
+print(55 / 2)
+print(2.5 * 2)
+print(8 - 25)
+
+# Integer Divison 
+print(8 // 5)
+# print(decimal.Decimal(10) / decimal.Decimal(3))
+# print(decimal.Decimal(0.365 - 0.3649))
+# print(math.pi)
+print(10 / 3)
+# Modulus (ნაშთის ოპერატორი)
+print(8 % 5)
+# Power (ახარისხება)
+
+print(2 ** 5)
+print("I am Luke", 20, "Years old", sep="")
